cfcf774655f0ec565dd8e840a6725028e195f0cb
RonInn <artking01kg@gmail.com>
2025-07-23 14:58:45 +0300

    feat: added home-manager

--- flake.nix
+++ flake.nix
@@ -3,12 +3,21 @@
 
 	inputs = {
 		nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+
+    home-manager = {
+      url = "github:nix-community/home-manager/release-25.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
 	};
 
-	outputs = {self, nixpkgs, ...}: {
+	outputs = {self, nixpkgs, home-manager, ...}: {
 		nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [  ./configuration.nix];
 		};
+    homeConfigurations.roninn = home-manager.lib.homeManagerConfiguration {
+      pkgs = nixpkgs.legacyPackages."x86_64-linux";
+      modules = [ ./home.nix];
+    };
 	};
 }
