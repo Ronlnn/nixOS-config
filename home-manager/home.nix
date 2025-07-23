bb46028b84902b8b9743bdcf66af19a148aadc0d
Ronlnn <artking01kg@gmail.com>
2025-07-23 21:50:17 +0300

    1

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -4,11 +4,7 @@
     ./modules/bundle.nix
     ./modules/wms/hyprland.nix
   ];
-  home = {
-    username = "roninn";
-    homeDirectory = "/home/roninn";
-    stateVersion = "25.05";
-  };
+  
   programs.git.enable = true;
   programs.zsh.enable = true;
   programs.bash.enable = true;
