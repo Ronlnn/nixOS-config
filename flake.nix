d58c8c5ca0e4c097f316a43bef87894dfe012cb0
RonInn <artking01kg@gmail.com>
2025-07-23 15:08:43 +0300

    fixed flake hm-manager

--- flake.nix
+++ flake.nix
@@ -1,23 +1,32 @@
 {
-	description = "System Configuration";
+  description = "System Configuration with Home Manager";
 
-	inputs = {
-		nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
-
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
     home-manager = {
       url = "github:nix-community/home-manager/release-25.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-	};
+  };
 
-	outputs = {self, nixpkgs, home-manager, ...}: {
-		nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [  ./configuration.nix];
-		};
-    homeConfigurations.roninn = home-manager.lib.homeManagerConfiguration {
-      pkgs = nixpkgs.legacyPackages."x86_64-linux";
-      modules = [ ./home.nix];
+  outputs = { self, nixpkgs, home-manager, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+    in {
+      nixosConfigurations.omen = nixpkgs.lib.nixosSystem {
+        inherit system;
+        modules = [
+          ./configuration.nix
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.roninn = import ./home.nix;
+          }
+        ];
+      };
     };
-	};
 }
