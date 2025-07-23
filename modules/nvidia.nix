446aa84ec74598533edfc5703dbce96c09dba945
RonInn <artking01kg@gmail.com>
2025-07-23 18:46:18 +0300

    nvidia

---
+++ modules/nvidia.nix
@@ -0,0 +1,24 @@
+{ config, pkgs, ... }:
+
+{
+  services.xserver.videoDrivers = [ "nvidia" ];
+
+  hardware.nvidia = {
+    modesetting.enable = true;
+    powerManagement.enable = true;
+    nvidiaSettings = true;
+    open = false; # GTX 1650 — проприетарный драйвер
+
+    prime = {
+      sync.enable = true;  # вместо offload
+      intelBusId = "PCI:0:2:0";
+      nvidiaBusId = "PCI:1:0:0";
+    };
+  };
+
+  # (опционально) добавить nvidia-smi, nvidia-settings
+  environment.systemPackages = with pkgs; [
+    glxinfo
+    nvidia-settings
+  ];
+}
