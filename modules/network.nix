0a8a50bba646c314f479be09e02a5de062bb9b9c
Ronlnn <artking01kg@gmail.com>
2025-07-24 11:35:28 +0300

    vpn

--- modules/network.nix
+++ modules/network.nix
@@ -1,4 +1,16 @@
 {
     # Enable networking
   networking.networkmanager.enable = true;
+  networking.enableIPv6 = true;
+  networking.useNetworkd = true;
+  boot.kernelModules = ["tun"];
+  networking.firewall.allowedUDPPorts = [8443];
+  networking.firewall.allowedTCPPorts = [1080 8443];
+
+  security.wrappers.xray = {
+    source = "/nix/store/your-xray-path/bin/xray";
+    owner = "root";
+    group = "root";
+    capabilities = "cap_net_admin,cap_net_raw+ep";
+  };
 }
