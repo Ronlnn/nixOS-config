32cf9bccd43cf6f3888e3b593e464ade9fa85c19
Ronlnn <artking01kg@gmail.com>
2025-07-24 15:08:14 +0300

    vpn

--- modules/network.nix
+++ modules/network.nix
@@ -1,44 +1,44 @@
-{config, pkgs, ...}:
+{ config, pkgs, lib, ... }:
+
+let
+  appDeps = [ pkgs.libpng pkgs.zlib pkgs.fuse3 ];
+in
 {
-    # Enable networking
   networking.networkmanager.enable = true;
   networking.enableIPv6 = true;
   networking.useNetworkd = false;
-  boot.kernelModules = ["tun" "fuse"];
+  boot.kernelModules = [ "tun" "fuse" ];
   networking.useDHCP = true;
-  #networking.firewall.allowedUDPPorts = [8443];
-  #networking.firewall.allowedTCPPorts = [1080 8443];
+
   networking.firewall.enable = false;
 
   security.wrappers.xray = {
-  source = "${pkgs.xray}/bin/xray";
-  owner = "root";
-  group = "root";
-  capabilities = "cap_net_admin,cap_net_raw+ep";
+    source = "${pkgs.xray}/bin/xray";
+    owner = "root";
+    group = "root";
+    capabilities = "cap_net_admin,cap_net_raw+ep";
+  };
 
-
-  # Переменные окружения для AppImage (чтобы видел нужные библиотеки)
   environment.sessionVariables = {
     LD_LIBRARY_PATH = lib.makeLibraryPath appDeps;
   };
 
-  # Включаем systemd-модуль user-tun (если используешь tun для VPN)
   systemd.services.nekoray = {
     description = "Nekoray VPN service";
     wantedBy = [ "multi-user.target" ];
     after = [ "network.target" ];
     serviceConfig = {
-      ExecStart = "${pkgs.nekoray}/bin/nekoray"; # или путь к твоему AppImage с appimage-run, если нужен
+      ExecStart = "${pkgs.nekoray}/bin/nekoray";
       Restart = "on-failure";
       CapabilityBoundingSet = "CAP_NET_ADMIN CAP_NET_BIND_SERVICE";
       AmbientCapabilities = "CAP_NET_ADMIN CAP_NET_BIND_SERVICE";
     };
   };
-    programs.nekoray = {
+
+  programs.nekoray = {
     enable = true;
     tunMode.enable = true;
   };
-  services.sing-box.enable = true;
-};
 
+  services.sing-box.enable = true;
 }
