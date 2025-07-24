98581660bff09718669228394b8180080749890f
Ronlnn <artking01kg@gmail.com>
2025-07-24 12:51:06 +0300

    vpn

--- modules/nekoray.nix
+++ modules/nekoray.nix
@@ -7,4 +7,12 @@
   };
   boot.kernelModules = [ "tun" ];
     networking.networkmanager.enable = true;
+
+  security.wrappers.nekobox_core = {
+  owner = "root";
+  group = "root";
+  source = "${pkgs.nekoray.nekobox-core}/bin/nekobox_core";
+  capabilities = "cap_net_admin+ep";
+};
+
 }
