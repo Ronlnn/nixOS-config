b82a1cdcc903db6ab364999d398c49ba0eed6942
Ronlnn <artking01kg@gmail.com>
2025-07-24 12:52:12 +0300

    vpn

--- modules/nekoray.nix
+++ modules/nekoray.nix
@@ -1,18 +1,20 @@
-{ pkgs, ... }:
+{ config, pkgs, lib, ... }:
 
 {
   programs.nekoray = {
     enable = true;
     tunMode.enable = true;
   };
+
+  # Обязательно, если используешь tun
   boot.kernelModules = [ "tun" ];
-    networking.networkmanager.enable = true;
+  networking.networkmanager.enable = true;
 
-  security.wrappers.nekobox_core = {
-  owner = "root";
-  group = "root";
-  source = "${pkgs.nekoray.nekobox-core}/bin/nekobox_core";
-  capabilities = "cap_net_admin+ep";
-};
-
+  # 🔧 Разрешаем конфликт через mkForce
+  security.wrappers.nekobox_core = lib.mkForce {
+    owner = "root";
+    group = "root";
+    source = "${pkgs.nekoray.nekobox-core}/bin/nekobox_core";
+    capabilities = "cap_net_admin+ep";
+  };
 }
