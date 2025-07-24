db04a726e3ad959c9c9580039942b9934c050ffa
Ronlnn <artking01kg@gmail.com>
2025-07-24 12:10:13 +0300

    vpn

--- modules/network.nix
+++ modules/network.nix
@@ -1,3 +1,4 @@
+{config, pkgs, ...}:
 {
     # Enable networking
   networking.networkmanager.enable = true;
@@ -8,9 +9,12 @@
   networking.firewall.allowedTCPPorts = [1080 8443];
 
   security.wrappers.xray = {
-    source = "/nix/store/your-xray-path/bin/xray";
-    owner = "root";
-    group = "root";
-    capabilities = "cap_net_admin,cap_net_raw+ep";
-  };
+  source = "${pkgs.xray}/bin/xray";
+  owner = "root";
+  group = "root";
+  capabilities = "cap_net_admin,cap_net_raw+ep";
+};
+
+
+
 }
