2f88a8b183ce953a26eb3f8d093bb05603a8f824
Ronlnn <artking01kg@gmail.com>
2025-07-24 17:02:42 +0300

    ff

--- modules/flatpak.nix
+++ modules/flatpak.nix
@@ -1,4 +1,20 @@
-{
+
+  {
   services.flatpak.enable = true;
   xdg.portal.enable = true;
+  # Автоматическое добавление путей в XDG_DATA_DIRS
+  systemd.user.services.flatpak-update = {
+    description = "Update Flatpak";
+    serviceConfig = {
+      Type = "oneshot";
+      ExecStart = "${pkgs.flatpak}/bin/flatpak update -y";
+    };
+  };
+
+  environment.sessionVariables = {
+    XDG_DATA_DIRS = [
+      "/var/lib/flatpak/exports/share"
+      "${config.home.homeDirectory}/.local/share/flatpak/exports/share"
+    ];
+  };
 }
