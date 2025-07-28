0c49374ddce31cc44a667326cb9e1720ad730bf5
Ronlnn <artking01kg@gmail.com>
2025-07-28 23:57:10 +0300

    login multi

--- modules/login.nix
+++ modules/login.nix
@@ -1,55 +1,27 @@
 { config, pkgs, ... }:
 
-let
-  hyprlandSession = ''
+{
+  services.xserver.enable = true;
+
+  services.displayManager.sddm.enable = true;
+
+  services.xserver.desktopManager.gnome.enable = true;
+
+  # Файл сессии Hyprland для выбора в SDDM
+  environment.etc."xdg/sessions/hyprland.desktop".text = ''
     [Desktop Entry]
     Name=Hyprland
     Comment=Hyprland Wayland compositor
     Exec=${pkgs.hyprland}/bin/Hyprland
     Type=Application
     DesktopNames=Hyprland
-    X-GDM-SessionRegisters=true
   '';
-in {
-  services.xserver.enable = true;
 
-  # Включаем SDDM
-  services.displayManager.sddm.enable = true;
-
-  # По умолчанию сессия на SDDM, можно выбрать вручную
-  services.displayManager.sddm.defaultSession = "hyprland";
-
-  # Включаем GNOME
-  services.xserver.desktopManager.gnome.enable = true;
-
-  # Добавляем сессию Hyprland в список SDDM
-  environment.etc."xdg/autostart/hyprland.desktop".text = hyprlandSession;
-
-  # Опционально: добавляем сессию вручную в папку сессий SDDM
-  environment.etc."xdg/sessions/hyprland.desktop".text = hyprlandSession;
-
-  # Настройки NVIDIA, если нужны
-  hardware.opengl.enable = true;
-  hardware.nvidia.modesetting.enable = true;
-  hardware.nvidia.powerManagement.enable = true;
-  hardware.nvidia.open = false;
-
-  # Переменные окружения для Wayland + NVIDIA (если нужно)
-  environment.sessionVariables = {
-    WLR_NO_HARDWARE_CURSORS = "1";
-    NIXOS_OZONE_WL = "1";
-    GBM_BACKEND = "nvidia-drm";
-    XDG_SESSION_TYPE = "wayland";
-    MOZ_ENABLE_WAYLAND = "1";
-  };
-
-  # Доп. пакеты для Wayland
+  # Дополнительные пакеты
   environment.systemPackages = with pkgs; [
     hyprland
     mako
     swayidle
     swaylock
-    grim
-    slurp
   ];
 }
