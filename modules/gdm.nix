bf50fc712c147b03dfc58c6f3ae459cbb29bee21
Ronlnn <artking01kg@gmail.com>
2025-07-29 19:04:38 +0300

    fix

--- modules/gdm.nix
+++ modules/gdm.nix
@@ -17,22 +17,17 @@
     papirus-icon-theme
   ];
 
-  # DConf нужен для настройки интерфейса GDM (как и для GNOME в целом)
-  programs.dconf.enable = true;
+environment.etc."gdm/greeter.dconf-defaults".text = ''
+  [org/gnome/desktop/interface]
+  gtk-theme='Catppuccin-Mocha-Standard-Blue-Dark'
+  icon-theme='Papirus-Dark'
+  cursor-theme='catppuccin-mocha-lavender-cursors'
+  color-scheme='prefer-dark'
 
-  # Настройки темы и курсора — это работает только для текущего пользователя,
-  # но мы можем применить и в GDM через login user — позже покажу как.
-  programs.dconf.settings = {
-    "org/gnome/desktop/interface" = {
-      gtk-theme = "Catppuccin-Mocha-Standard-Blue-Dark";
-      icon-theme = "Papirus-Dark";
-      cursor-theme = "catppuccin-mocha-lavender-cursors";
-      color-scheme = "prefer-dark";
-    };
-    "org/gnome/desktop/background" = {
-      picture-uri = "file:///home/roninn/Pictures/Walls/rei.jpg";
-    };
-  };
+  [org/gnome/desktop/background]
+  picture-uri='file:///home/roninn/Pictures/Walls/rei.jpg'
+'';
+
 
   # Если нужен авто-логин (по желанию)
   # services.xserver.displayManager.autoLogin.enable = true;
