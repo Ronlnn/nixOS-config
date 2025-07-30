6ee534b65c253df6892f2caee62cdb978cfa77f4
Ronlnn <artking01kg@gmail.com>
2025-07-31 01:24:12 +0300

    sys backup

--- home-manager/modules/alacritty.nix
+++ home-manager/modules/alacritty.nix
@@ -1,12 +1,6 @@
 {
   programs.alacritty = {
     enable = true;
-    settings = {
-      window.dimensions = {
-        lines = 3;
-        columns = 200;
-      };
-    };
     theme = "dracula";
   };
 }
