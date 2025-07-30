82801ee202f774a3aea54faf4aa2776d088cf035
Ronlnn <artking01kg@gmail.com>
2025-07-31 01:33:04 +0300

    sys backup

--- home-manager/modules/wms/hyprland.nix
+++ home-manager/modules/wms/hyprland.nix
@@ -8,7 +8,7 @@
 
       settings = {
         bind = [
-          "SUPER, RETURN, exec, alacritty"
+          "SUPER,RETURN,exec,/etc/profiles/per-user/roninn/bin/alacritty"
         ];
       };
     };
