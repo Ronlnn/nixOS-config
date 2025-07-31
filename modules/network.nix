cea18414b7145a92ea4b5e590571219ce2dd0060
Ronlnn <artking01kg@gmail.com>
2025-07-31 13:08:23 +0300

    sys backup

--- modules/network.nix
+++ modules/network.nix
@@ -6,14 +6,13 @@
  # networking.enableIPv6 = true;
   #networking.useNetworkd = false;
   #boot.kernelModules = [ "tun" "fuse" ];
-  networking.useDHCP = true;
+  networking.useDHCP = lib.mkForce false;
 
-  # security.wrappers.xray = {
-  #   source = "${pkgs.xray}/bin/xray";
-  #   owner = "root";
-  #   group = "root";
-  #   capabilities = "cap_net_admin,cap_net_raw+ep";
-  # };
+  networking.networkmanager.dns = "systemd-resolved";
+
+  services.resolved.enable = true;
+
+
 
 
 }
