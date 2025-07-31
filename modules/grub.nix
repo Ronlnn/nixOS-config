c98050023ba84b85d34adb3112853c264b8949b3
Ronlnn <artking01kg@gmail.com>
2025-08-01 00:48:23 +0300

    sys backup

--- modules/grub.nix
+++ modules/grub.nix
@@ -12,9 +12,7 @@
       theme =  "${sekiroTheme}/grub/themes/sekiro";
       gfxmodeEfi = "1920x1080";
 
-      font = if builtins.pathExists "${sekiroTheme}/grub/themes/sekiro/dersu_uzala_brush_16.pf2"
-           then "${sekiroTheme}/grub/themes/sekiro/dersu_uzala_brush_16.pf2"
-           else null;
+     
       #splashImage = "${sekiroTheme}/themes/sekiro/background.png";
 
     };
--- packages/sekiro-theme-grub.nix
+++ packages/sekiro-theme-grub.nix
@@ -1,37 +1,36 @@
 { stdenv, fetchFromGitHub }:
 
-stdenv.mkDerivation {
+stdenv.mkDerivation rec {
   pname = "sekiro-grub-theme";
   version = "1.0";
 
   src = fetchFromGitHub {
     owner = "AbijithBalaji";
     repo = "sekiro_grub_theme";
-    rev = "1b1e3840e9c378f4400bed2a8940f4ded364ba3f"; # пример
+    rev = "1b1e3840e9c378f4400bed2a8940f4ded364ba3f";
     sha256 = "sha256-uXwDjb0+ViQvdesG5gefC5zFAiFs/FfDfeI5t7vP+Qc=";
   };
 
-  sourceRoot =  "sourceRoot=$(echo $PWD/Sekiro)";
+  # Указываем правильный путь к файлам темы
+  preConfigure = ''
+    cd Sekiro
+  '';
 
   installPhase = ''
     # Создаём стандартную структуру GRUB темы
     mkdir -p $out/grub/themes/sekiro
 
     # Копируем все файлы темы
-    cp -r * $out/grub/themes/sekiro/
+    cp -r ./* $out/grub/themes/sekiro/
 
     # Проверяем наличие обязательных файлов
     if [ ! -f "$out/grub/themes/sekiro/theme.txt" ]; then
-      echo "ERROR: theme.txt not found!"
+      echo "ERROR: theme.txt not found in $out/grub/themes/sekiro/"
+      ls -la $out/grub/themes/sekiro/
       exit 1
     fi
 
-    if [ ! -f "$out/grub/themes/sekiro/sekiro_1920x1080.png" ]; then
-      echo "ERROR: background image not found!"
-      exit 1
-    fi
-
-    echo "Theme files:"
+    echo "Theme installed successfully:"
     ls -la $out/grub/themes/sekiro/
   '';
 }
