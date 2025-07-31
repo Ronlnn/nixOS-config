3d7dfc4adf8808c9b8da9d565d54a74d4f8c1d47
Ronlnn <artking01kg@gmail.com>
2025-07-31 20:23:50 +0300

    sys backup

--- modules/grub.nix
+++ modules/grub.nix
@@ -1,11 +1,17 @@
 {pkgs, ...}:
+
+  let
+    sekiroTheme = import ../packages/sekiro-theme-grub.nix{
+      inherit (pkgs) stdenv fetchFromGitHub;
+    };
+  in
 {
- boot.loader.grub = {
+  boot.loader.grub = {
   enable = true;
   efiSupport = true;
   device = "nodev";  # для UEFI
   #splashImage = ../walls/zenitsu.png;
-  theme = "/boot/grub/themes/sekiro_grub_theme/theme.txt";
+  theme = "${sekiroTheme}/theme.txt";
   };
   boot.loader.efi.canTouchEfiVariables = true;
 
--- packages/sekiro-theme-grub.nix
+++ packages/sekiro-theme-grub.nix
@@ -1,17 +1,18 @@
-{config, pkgs, ...}:
-{
-   sekiroGrubTheme = pkgs.stdenv.mkDerivation {
-    name = "sekiro-grub-theme";
-    src = pkgs.fetchFromGitHub {
-      owner = "AbijithBalaji";
-      repo = "sekiro_grub_theme";
-      rev = "1b1e3840e9c378f4400bed2a8940f4ded364ba3f"; # фиксируй на конкретный коммит!
-      sha256 = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA="; # см. ниже
-    };
+{ stdenv, fetchFromGitHub }:
 
-    installPhase = ''
-      mkdir -p $out
-      cp -r * $out/
-    '';
+stdenv.mkDerivation {
+  pname = "sekiro-grub-theme";
+  version = "1.0";
+
+  src = fetchFromGitHub {
+    owner = "AbijithBalaji";
+    repo = "sekiro_grub_theme";
+    rev = "1b1e3840e9c378f4400bed2a8940f4ded364ba3f"; # пример
+    sha256 = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
   };
+
+  installPhase = ''
+    mkdir -p $out
+    cp -r * $out/
+  '';
 }
