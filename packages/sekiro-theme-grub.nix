4364c326db1f95f0141312b8f1e852ba2e24aeb1
Ronlnn <artking01kg@gmail.com>
2025-07-31 20:24:35 +0300

    sys backup

--- packages/sekiro-theme-grub.nix
+++ packages/sekiro-theme-grub.nix
@@ -8,7 +8,7 @@ stdenv.mkDerivation {
     owner = "AbijithBalaji";
     repo = "sekiro_grub_theme";
     rev = "1b1e3840e9c378f4400bed2a8940f4ded364ba3f"; # пример
-    sha256 = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
+    sha256 = "sha256-uXwDjb0+ViQvdesG5gefC5zFAiFs/FfDfeI5t7vP+Qc=";
   };
 
   installPhase = ''
