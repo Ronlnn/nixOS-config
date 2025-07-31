04e2b24824671280a3f9a8d7e7c64ce690314e34
Ronlnn <artking01kg@gmail.com>
2025-07-31 19:24:58 +0300

    sys backup

--- modules/bootloader.nix
+++ modules/bootloader.nix
@@ -1,5 +1,18 @@
 {
    # Bootloader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+  #boot.loader.systemd-boot.enable = true;
+  #boot.loader.efi.canTouchEfiVariables = true;
+  #boot.loader.timeout = 3;
+  boot.loader.limine = {
+  enable = true;
+  configurationLimit = 5;
+  style = {
+    enable = true;
+    resolution = "1920x1080";
+    wallpapers = [
+      ../walls/zenitsu.png
+    ];
+  };
+};
+
 }
