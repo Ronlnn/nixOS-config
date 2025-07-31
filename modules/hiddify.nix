b68d5f90a8dcc36f3930bfd84b0a7e35b3c9908b
Ronlnn <artking01kg@gmail.com>
2025-07-31 14:24:28 +0300

    sys backup

--- modules/hiddify.nix
+++ modules/hiddify.nix
@@ -1,12 +1,12 @@
 { config, pkgs, ... }:
 
 {
-    environment.systemPackages = [ pkgs.hiddify-app ];
+
     systemd.services.hiddify = {
     enable = true;
     wantedBy = [ "multi-user.target" ];
     serviceConfig = {
-      ExecStart = "${pkgs.hiddify}/bin/hiddify";  # ✅ Лучший вариант
+      ExecStart = "${pkgs.hiddify-app}/bin/hiddify";  # ✅ Лучший вариант
       # ИЛИ, если пакет называется иначе:
       # ExecStart = "${pkgs.hiddify-manager}/bin/hiddify";
       User = "root";  # (или лучше создать отдельного пользователя)
