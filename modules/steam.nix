8f41ffe5c050ded12453da00c040facc201c21b2
Ronlnn <artking01kg@gmail.com>
2025-08-01 01:06:13 +0300

    Steam

---
+++ modules/steam.nix
@@ -0,0 +1,15 @@
+{pkgs, ...}:
+{
+  programs.steam.enable = true;
+  programs.steam.gamescopeSession.enable = true;
+  programs.gamemode.enable = true;
+
+  environment.systemPackages = with pkgs; [
+    protonup
+  ];
+
+  environment.sessionVariables = {
+    STEAM_EXTRA_COMPAT_TOOLS_PATHS =
+      "/home/roninn/.steam/root/compatibilitytools.d";
+  };
+}
