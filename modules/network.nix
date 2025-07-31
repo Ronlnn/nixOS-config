ed56afd64ed7ba2eafa3f0a05bc59a4f6139440e
Ronlnn <artking01kg@gmail.com>
2025-07-31 11:25:31 +0300

    sys backup

--- modules/network.nix
+++ modules/network.nix
@@ -5,17 +5,17 @@ let
 in
 {
   networking.networkmanager.enable = true;
-  networking.enableIPv6 = true;
-  networking.useNetworkd = false;
-  boot.kernelModules = [ "tun" "fuse" ];
+ # networking.enableIPv6 = true;
+  #networking.useNetworkd = false;
+  #boot.kernelModules = [ "tun" "fuse" ];
   #networking.useDHCP = true;
 
-  security.wrappers.xray = {
-    source = "${pkgs.xray}/bin/xray";
-    owner = "root";
-    group = "root";
-    capabilities = "cap_net_admin,cap_net_raw+ep";
-  };
+  # security.wrappers.xray = {
+  #   source = "${pkgs.xray}/bin/xray";
+  #   owner = "root";
+  #   group = "root";
+  #   capabilities = "cap_net_admin,cap_net_raw+ep";
+  # };
 
 
 }
