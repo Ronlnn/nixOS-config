335d0d71106f9c13f649bba06ac46bd01a4b87a2
Ronlnn <artking01kg@gmail.com>
2025-07-31 14:20:34 +0300

    sys backup

--- modules/hiddify.nix
+++ modules/hiddify.nix
@@ -1,35 +1,15 @@
 { config, pkgs, ... }:
 
 {
-  environment.systemPackages = [ pkgs.hiddify-app ];
-
-  systemd.services.hiddify-vpn = {
+    systemd.services.hiddify = {
     enable = true;
-    description = "Hiddify VPN Service";
     wantedBy = [ "multi-user.target" ];
-    after = [ "network.target" ];
-
     serviceConfig = {
-      Type = "simple";
-      # Используем правильное имя бинарника (выберите один вариант):
-      ExecStart = "${pkgs.hiddify-app}/bin/hiddify --vpn-mode";  # Вариант 1
-      # ИЛИ
-      # ExecStart = "${pkgs.hiddify-app}/bin/HiddifyCli --vpn-mode";  # Вариант 2
-
+      ExecStart = "${pkgs.hiddify}/bin/hiddify";  # ✅ Лучший вариант
+      # ИЛИ, если пакет называется иначе:
+      # ExecStart = "${pkgs.hiddify-manager}/bin/hiddify";
+      User = "root";  # (или лучше создать отдельного пользователя)
       Restart = "on-failure";
-      User = "root";
-      Group = "root";
-      CapabilityBoundingSet = [ "CAP_NET_ADMIN" "CAP_NET_RAW" ];
-      AmbientCapabilities = [ "CAP_NET_ADMIN" "CAP_NET_RAW" ];
-
-      # Добавляем необходимые переменные окружения
-      Environment = [
-        "PATH=${pkgs.coreutils}/bin:${pkgs.iproute2}/bin:${pkgs.iptables}/bin"
-        "LD_LIBRARY_PATH=${pkgs.stdenv.cc.cc.lib}/lib"
-      ];
-
-      # Рабочая директория
-      WorkingDirectory = "/tmp";
     };
   };
 }
