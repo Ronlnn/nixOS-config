0313086ee92d85fe6f4a92489170d082f1725fb0
Ronlnn <artking01kg@gmail.com>
2025-08-01 00:58:45 +0300

    Grub теперь красивый

--- modules/grub.nix
+++ modules/grub.nix
@@ -8,13 +8,8 @@
       enable = true;
       efiSupport = true;
       device = "nodev";
-
       theme =  "${sekiroTheme}/grub/themes/sekiro";
       gfxmodeEfi = "1920x1080";
-
-     
-      #splashImage = "${sekiroTheme}/themes/sekiro/background.png";
-
     };
     boot.loader.efi.canTouchEfiVariables = true;
 
