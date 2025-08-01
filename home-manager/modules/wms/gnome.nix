5ba318c10a02797caad3f1058ef0269262cb0f3c
Ronlnn <artking01kg@gmail.com>
2025-08-01 12:23:46 +0300

    gnome

--- home-manager/modules/wms/gnome.nix
+++ home-manager/modules/wms/gnome.nix
@@ -1,31 +1,32 @@
-{config, lib, pkgs, ...}:
-{
-  lib.mkIf config.gnome.enable {
-    programs.dconf.enable = true;
-    home.packages = with pkgs; [
-      dracula-theme
-      catppuccin-gtk
-      qogir-icon-theme
-      tela-icon-theme
-      papirus-icon-theme
-      bibata-cursors
-      gnomeExtensions.user-themes
-    ];
-    #Включаем расширение user-themes
-    dconf.settings = {
-      "org/gnome/shell" = {
-        enabled-extensions = [ "user-theme@gnome-shell-extensions.gcampax.github.com" ];
-      };
-          # Включаем нужную тему
-      "org/gnome/shell/extensions/user-theme" = {
-        name = "Catppuccin-Mocha-Standard-Blue-Dark";
-      };
-      "org/gnome/desktop/interface" = {
-        gtk-theme = "Catppuccin-Mocha-Standard-Blue-Dark";
-        icon-theme = "Papirus-Dark";
-        cursor-theme = "Bibata-Modern-Classic";
-        font-name = "Inter 10";
-      };
+{ config, lib, pkgs, ... }:
+
+lib.mkIf config.gnome.enable {
+  programs.dconf.enable = true;
+
+  home.packages = with pkgs; [
+    dracula-theme
+    catppuccin-gtk
+    qogir-icon-theme
+    tela-icon-theme
+    papirus-icon-theme
+    bibata-cursors
+    gnomeExtensions.user-themes
+  ];
+
+  dconf.settings = {
+    "org/gnome/shell" = {
+      enabled-extensions = [ "user-theme@gnome-shell-extensions.gcampax.github.com" ];
+    };
+
+    "org/gnome/shell/extensions/user-theme" = {
+      name = "Catppuccin-Mocha-Standard-Blue-Dark";
+    };
+
+    "org/gnome/desktop/interface" = {
+      gtk-theme = "Catppuccin-Mocha-Standard-Blue-Dark";
+      icon-theme = "Papirus-Dark";
+      cursor-theme = "Bibata-Modern-Classic";
+      font-name = "Inter 10";
     };
   };
 }
