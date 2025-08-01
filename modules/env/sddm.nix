fa18b5f9c4ab4daee507089b7a7bbb48adb59ec0
Ronlnn <artking01kg@gmail.com>
2025-08-01 21:32:57 +0300

    sddm-catppuccin

--- modules/env/sddm.nix
+++ modules/env/sddm.nix
@@ -1,19 +1,12 @@
 { config, lib, pkgs, ... }:
-let
-  sddmAstronautTheme = pkgs.fetchFromGitHub {
-    owner = "Keyitdev";
-    repo = "sddm-astronaut-theme";
-    rev = "3ef9f511fd072ff3dbb6eb3c1c499a71f338967e";
-    sha256 = "sha256-33CzZ4vK1dicVzICbudk8gSRC/MExG+WnrE9wIWET14=";
-  };
-in {
-
-  environment.etc."sddm/themes/astronaut".source = sddmAstronautTheme;
-
+{
+  environment.systemPackages = [
+    pkgs.catppuccin-sddm
+  ];
   services.displayManager.sddm = lib.mkIf config.hyprland.enable {
     enable = true;
     wayland.enable = true;
-    theme = "astronaut";
+    theme = "catppuccin";
   };
 }
 
