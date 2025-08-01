fc05791b5b9aeb40e00a6c4176cd00b17699f138
Ronlnn <artking01kg@gmail.com>
2025-08-01 12:20:01 +0300

    gnome gtk

--- home-manager/modules/wms/gnome.nix
+++ home-manager/modules/wms/gnome.nix
@@ -16,16 +16,16 @@
       "org/gnome/shell" = {
         enabled-extensions = [ "user-theme@gnome-shell-extensions.gcampax.github.com" ];
       };
-    };
-    # Включаем нужную тему
-    "org/gnome/shell/extensions/user-theme" = {
-      name = "Catppuccin-Mocha-Standard-Blue-Dark";
-    };
-    "org/gnome/desktop/interface" = {
-      gtk-theme = "Catppuccin-Mocha-Standard-Blue-Dark";
-      icon-theme = "Papirus-Dark";
-      cursor-theme = "Bibata-Modern-Classic";
-      font-name = "Inter 10";
+          # Включаем нужную тему
+      "org/gnome/shell/extensions/user-theme" = {
+        name = "Catppuccin-Mocha-Standard-Blue-Dark";
+      };
+      "org/gnome/desktop/interface" = {
+        gtk-theme = "Catppuccin-Mocha-Standard-Blue-Dark";
+        icon-theme = "Papirus-Dark";
+        cursor-theme = "Bibata-Modern-Classic";
+        font-name = "Inter 10";
+      };
     };
   };
 }
