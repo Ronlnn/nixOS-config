81e50b91ba9a16fcb35e3eed4fa8dbd9826cfd33
Ronlnn <artking01kg@gmail.com>
2025-08-01 16:11:05 +0300

    а

--- modules/env/gdm.nix
+++ modules/env/gdm.nix
@@ -8,19 +8,5 @@
       enable = true;
       wayland = true; # по желанию, можно отключить
     };
-
-    # Установка системных тем, иконок и курсоров
-    environment.systemPackages = with pkgs; [
-      pkgs.gnome-themes-extra
-      pkgs.adwaita-icon-theme
-      catppuccin-gtk
-      catppuccin-cursors.mochaLavender
-      papirus-icon-theme
-    ];
-
-
-    # Если нужен авто-логин (по желанию)
-    # services.xserver.displayManager.autoLogin.enable = true;
-    # services.xserver.displayManager.autoLogin.user = "roninn";
   };
 }
