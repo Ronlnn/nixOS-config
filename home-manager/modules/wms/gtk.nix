c6c50b3e9ba558785a26581893f6f108c50e6cf3
Ronlnn <artking01kg@gmail.com>
2025-08-01 12:58:33 +0300

    gtk

--- home-manager/modules/wms/gtk.nix
+++ home-manager/modules/wms/gtk.nix
@@ -6,10 +6,10 @@
   gtk.cursorTheme.name = "Bibata-Modern-Ice";
 
   gtk.theme.package = pkgs.catppuccin-gtk;
-  gtk.theme.name = "Catppuccin";
+  gtk.theme.name = "Catppuccin-Mocha-Standard-Blue-Dark";
 
   gtk.iconTheme.package = pkgs.tela-icon-theme;
-  gtk.iconTheme.name = "Tela";
+  gtk.iconTheme.name = "Tela-dark";
 
 
 }
