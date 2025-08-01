0adecedcfca1623da45b852a1db7cd448dcd2924
Ronlnn <artking01kg@gmail.com>
2025-08-01 12:23:04 +0300

    gnome

--- home-manager/modules/wms/gnome.nix
+++ home-manager/modules/wms/gnome.nix
@@ -1,6 +1,6 @@
 {config, lib, pkgs, ...}:
 {
-  config = lib.mkIf config.gnome.enable {
+  lib.mkIf config.gnome.enable {
     programs.dconf.enable = true;
     home.packages = with pkgs; [
       dracula-theme
