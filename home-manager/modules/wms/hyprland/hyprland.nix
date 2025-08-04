07899ed0ab092eb613e46f14a6b8ad94d87bdf55
Ronlnn <artking01kg@gmail.com>
2025-08-04 17:29:27 +0300

    hypr

--- home-manager/modules/wms/hyprland/hyprland.nix
+++ home-manager/modules/wms/hyprland/hyprland.nix
@@ -56,7 +56,7 @@
           ignore_opacity = true;
           new_optimizations = true;
           xray = true;
-          noise = "0.011"7;
+          noise = "0.0117";
           contrast = "0.8916";
           brightness = "0.8172";
           vibrancy_darkness = "0.0";
