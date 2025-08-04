9eca5b434b46991650a167810bf45bdd8387a705
Ronlnn <artking01kg@gmail.com>
2025-08-05 00:27:59 +0300

    up hypr

--- flake.nix
+++ flake.nix
@@ -11,7 +11,7 @@
     hyprland.url = "github:hyprwm/Hyprland";
 
 };
-  outputs = { self, nixpkgs, home-manager, hyprland, ... } :
+  outputs = inputs@{ self, nixpkgs, home-manager, hyprland, ... } :
 
     let
       system = "x86_64-linux";
