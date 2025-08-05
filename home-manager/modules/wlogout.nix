33bcb5408820f4583c048b3946e41cb62e09389c
Ronlnn <artking01kg@gmail.com>
2025-08-05 16:00:32 +0300

    ff

--- home-manager/modules/wlogout.nix
+++ home-manager/modules/wlogout.nix
@@ -18,16 +18,16 @@
     enable = true;
     style = ''
       window {
-        background_color = #1E1E2E
+        background_color: #1E1E2E;
       }
       button {
-        border-radius = 8
-        border-color = #6E6C7E
-        border-style = solid
-        border-width = 2
-        color = #C6A0F6
-        font-family=  JetBrainsMono Nerd Font
-        font-size = 12
+        border-radius: 8px;
+        border-color: #6E6C7E;
+        border-style: solid;
+        border-width: 2px;
+        color: #C6A0F6;
+        font-family: "JetBrainsMono Nerd Font";
+        font-size: 12px;
       }
     '';
   };
