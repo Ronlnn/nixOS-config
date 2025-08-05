dcf7fd8df04d4261c0a6582089b54ef297081463
Ronlnn <artking01kg@gmail.com>
2025-08-05 15:41:23 +0300

    wlogout

--- home-manager/modules/wlogout.nix
+++ home-manager/modules/wlogout.nix
@@ -1,21 +1,24 @@
 {
   programs.wlogout.enable = true;
 
-  programs.wlogout.layout = {
-    label = {
-      font = "JetBrainsMono Nerd Font 12"; # можно свой шрифт
-      color = "#C6A0F6"; # цвет текста — возьмём из палитры Catppuccin Mocha (lavender)
-    };
-
-    style = {
-      background = "#1E1E2E";  # Catppuccin Mocha background
-      border = "#6E6C7E";      # border color из палитры (overlay2)
-      borderWidth = "2";
-      radius = "8";            # радиус скругления углов
-      buttonColor = "#FAB387"; # цвет кнопок (peach)
-      buttonHoverColor = "#F2CDCD"; # цвет кнопок при наведении
-      labelColor = "#C6A0F6";  # цвет текста на кнопках
-      shadow = "0 0 15px #00000088"; # небольшой теневой эффект
-    };
-  };
+  programs.wlogout.layout = [
+    {
+      label = {
+        font = "JetBrainsMono Nerd Font 12";
+        color = "#C6A0F6";  # lavender
+      };
+    }
+    {
+      style = {
+        background = "#1E1E2E";  # mocha background
+        border = "#6E6C7E";      # overlay2 border
+        borderWidth = "2";
+        radius = "8";
+        buttonColor = "#FAB387";       # peach
+        buttonHoverColor = "#F2CDCD";  # lighter peach
+        labelColor = "#C6A0F6";        # lavender
+        shadow = "0 0 15px #00000088";
+      };
+    }
+  ];
 }
