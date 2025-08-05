d60718e0d10302c7c5878171b671858810f2cd9c
Ronlnn <artking01kg@gmail.com>
2025-08-05 16:59:05 +0300

    ff

--- home-manager/modules/wms/hyprland.nix
+++ home-manager/modules/wms/hyprland.nix
@@ -59,12 +59,12 @@
 
     extraConfig = ''
 
-        workspace = 1, monitor:HDMI-A-2, default:true
+        workspace = 1, monitor:HDMI-A-2
         workspace = 2, monitor:HDMI-A-2
         workspace = 3, monitor:HDMI-A-2
         workspace = 4, monitor:HDMI-A-2
         workspace = 5, monitor:HDMI-A-2
-        workspace = 6, monitor:eDP-1, default:true
+        workspace = 6, monitor:eDP-1
         workspace = 7, monitor:eDP-1
         workspace = 8, monitor:eDP-1
         workspace = 9, monitor:eDP-1
