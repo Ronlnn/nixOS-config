f4bd40aa03dcbf5d9593a4cb4db4e0da6a55f149
Ronlnn <artking01kg@gmail.com>
2025-08-05 19:46:10 +0300

    sddm

--- modules/env/sddm.nix
+++ modules/env/sddm.nix
@@ -1,15 +1,26 @@
 {config, lib, pkgs,...}:
+
+let
+  customBackground = /home/roninn/Pictures/Walls/purp.jpg;
+
+  customTheme = pkgs.catppuccin-sddm.override {
+    background = customBackground;
+    blur = true;
+  };
+in
 {
   config = lib.mkIf config.hyprland.enable {
     services.displayManager.sddm = {
       enable = true;
       wayland.enable = true;
-      theme = "catppuccin-mocha";
+      theme = "catppuccin-mocha-custom";
       package = pkgs.kdePackages.sddm;
+
     };
     environment.systemPackages = with pkgs; [
       catppuccin-sddm
       sddm-astronaut
+      customTheme
     ];
   };
 }
