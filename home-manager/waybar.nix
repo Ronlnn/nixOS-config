8d754e11e48b40b5bcfd771bfe9b9dfc84f6f43b
Ronlnn <artking01kg@gmail.com>
2025-08-06 12:07:46 +0300

    waybar

--- home-manager/waybar.nix
+++ home-manager/waybar.nix
@@ -32,7 +32,7 @@
     };
     style = ''
       window#waybar {
-        background: rgba(64, 30, 135);
+        background: rgba(64, 30, 135,1);
         border: 1px solid #E8E546;
         border-radius: 5px;
       }
