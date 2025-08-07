11739c29a2a3f47dfbe3652ef31d8bc6e229beec
Ronlnn <artking01kg@gmail.com>
2025-08-08 01:30:31 +0300

    ff

--- home-manager/waybar.nix
+++ home-manager/waybar.nix
@@ -48,7 +48,7 @@
         "backlight" = {
           device = "intel_backlight";
           scroll-step = 10.0;
-          format = "<big>{icon}</big>  {percent}%";
+          format = " <big>{icon}</big>  {percent}% ";
           format-icons = ["<big>󱩎</big> " "<big>󱩒</big> " "<big>󱩔</big> " "<big>󱩖</big>"];
         };
         "pulseaudio" = {
