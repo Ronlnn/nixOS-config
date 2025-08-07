9cb0ad4ce86229e963be5731feb97f2bb25f69b8
Ronlnn <artking01kg@gmail.com>
2025-08-08 01:12:38 +0300

    vol

--- home-manager/waybar.nix
+++ home-manager/waybar.nix
@@ -13,7 +13,7 @@
         height = 40;
         modules-left = ["hyprland/workspaces"];
         modules-center = ["user" "clock" "cava"];
-        modules-right = ["backlight" "cpu" "memory" "battery" "bluetooth"];
+        modules-right = ["backlight" "pulseaudio" "cpu" "memory" "battery" "bluetooth"];
         "hyprland/workspaces" = {
           format = "{icon}";
           format-icons = {
