b9368fa89c87cc6799b4261a50c581234ac68b90
Ronlnn <artking01kg@gmail.com>
2025-08-08 18:18:10 +0300

    ff

--- home-manager/waybar.nix
+++ home-manager/waybar.nix
@@ -278,13 +278,11 @@
 
       /* System */
       #custom-system {
-        min-width: 50px;
-        font-size: 19px;
+        font-size: 20px;
         color: #62F0AD;
         background: #1E1E2E;
         border-radius: 20px;
         margin: 4px;
-        padding: 0 4px;
       }
 
       /* Workspaces */
