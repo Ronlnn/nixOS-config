bfeb87bb9d6bb15945b515a74610b49866152ee1
Ronlnn <artking01kg@gmail.com>
2025-08-08 17:34:04 +0300

    ff

--- configuration.nix
+++ configuration.nix
@@ -20,12 +20,15 @@
 
   time.timeZone = "Europe/Moscow";
 
-  i18n.defaultLocale = "en_US.UTF-8";
-  i18n.supportedLocales = "ru_RU.UTF-8/UTF-8";
 
   services.xserver = {
     layout = "us,ru";
     xkbOptions = "grp:alt_shift_toggle";
+  };
+
+  i18n = {
+    defaultLocale = "en_US.UTF-8";
+    supportedLocales = ["en_US.UTF-8/UTF-8" "ru_RU.UTF-8/UTF-8"];
   };
 
   i18n.extraLocaleSettings = {
