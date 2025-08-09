8747de61e1d3b3da8dbbee1470fbbb2690cddad2
Ronlnn <artking01kg@gmail.com>
2025-08-09 13:04:19 +0300

    swaylock

--- home-manager/swaylock.nix
+++ home-manager/swaylock.nix
@@ -8,12 +8,12 @@
       indicator-idle-visible = true;
       indicator-radius = 100;
       show-failed-attempts = false;
-      image = "~/Pictures/Walls/samurai-3.jpg";
+      image = "~/Pictures/Walls/rei.jpg";
 
       # Используем правильный формат цветов (RRGGBBAA)
-      ring-color = "54E8D700";    # Прозрачное кольцо (AA=00)
-      inside-color = "54E8D7BF";  # Полупрозрачная внутренняя часть (BF = 75%)
-      line-color = "54E8D7BF";    # Полупрозрачная линия (BF = 75%)
+      ring-color = "E8546000";    # Прозрачное кольцо (AA=00)
+      inside-color = "1E1E2EBF";  # Полупрозрачная внутренняя часть (BF = 75%)
+      line-color = "E85460BF";    # Полупрозрачная линия (BF = 75%)
       separator-color = "00000000"; # Прозрачный разделитель
     };
   };
