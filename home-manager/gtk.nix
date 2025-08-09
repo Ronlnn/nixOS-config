feec0c2485785ae2378165c116c00725acf3dad9
Ronlnn <artking01kg@gmail.com>
2025-08-09 16:03:36 +0300

    ff

--- home-manager/gtk.nix
+++ home-manager/gtk.nix
@@ -1,17 +1,23 @@
 { config, pkgs, ... }:
 
 {
+  # 1. Установка необходимых тем и инструментов
   home.packages = with pkgs; [
+    # Тема для GTK/Qt
     catppuccin-gtk
+    # Иконки
     papirus-icon-theme
+    # Курсоры
     bibata-cursors
+    # Инструмент для настройки Qt
     qt6ct
-    libsForQt5.qt5ct  # Для совместимости с Qt5
   ];
 
-  # Настройка GTK
+  # 2. Настройка GTK (для Thunar, Blueman, Pavucontrol)
   gtk = {
     enable = true;
+
+    # Тема Catppuccin Mocha (темная)
     theme = {
       name = "Catppuccin-Mocha-Standard-Blue-Dark";
       package = pkgs.catppuccin-gtk.override {
@@ -20,30 +26,40 @@
         variant = "mocha";
       };
     };
+
+    # Иконки Papirus
     iconTheme = {
-      name = "Papirus-Dark";
-      package = pkgs.papirus-icon-theme;
+      name = "Tela-dark";
+      package = pkgs.tela-icon-theme;
     };
+
+    # Курсоры Bibata Modern
     cursorTheme = {
-      name = "Bibata-Modern-Classic";
+      name = "Bibata-Modern-Ice";
       package = pkgs.bibata-cursors;
       size = 24;
     };
+
+    # Настройки для GTK 3/4
     gtk3.extraConfig = {
       gtk-application-prefer-dark-theme = 1;
     };
+
     gtk4.extraConfig = {
       gtk-application-prefer-dark-theme = 1;
     };
   };
 
 
+  # 4. Настройка окружения
   home.sessionVariables = {
+    # Принудительно включаем темную тему
     GTK_THEME = "Catppuccin-Mocha-Standard-Blue-Dark";
+
   };
+z
 
-
-  # Настройка Thunar
+  # 6. Настройка для отдельных приложений
   xdg.configFile."Thunar/gtkrc".text = ''
     style "thunar-style" {
       ThunarIconView::label-alpha = 0
