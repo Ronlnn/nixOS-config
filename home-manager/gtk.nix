9610caa6c19fe9f27b0af443405aef449f844367
Ronlnn <artking01kg@gmail.com>
2025-08-10 13:43:54 +0300

    ff

--- home-manager/gtk.nix
+++ home-manager/gtk.nix
@@ -37,21 +37,21 @@
       size = 24;
     };
 
-    gtk.gtk2.extraConfig = ''
-      gtk-theme-name="Dracula"
-      gtk-icon-theme-name="Tela-dark"
-      gtk-cursor-theme-name="Bibata-Modern-Ice"
-      gtk-cursor-theme-size=24
-    '';
+    gtk.gtk2.extraConfig = {
+      gtk-theme-name="Dracula";
+      gtk-icon-theme-name="Tela-dark";
+      gtk-cursor-theme-name="Bibata-Modern-Ice";
+      gtk-cursor-theme-size=24;
+    };
 
     # Настройки для GTK 3/4
-    gtk3.extraConfig = ''
+    gtk3.extraConfig = {
       gtk-application-prefer-dark-theme = 1;
-    '';
+    };
 
-    gtk4.extraConfig = ''
+    gtk4.extraConfig = {
       gtk-application-prefer-dark-theme = 1;
-    '';
+    };
   };
 
 
