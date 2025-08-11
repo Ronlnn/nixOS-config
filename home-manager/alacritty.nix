123c8e9f63e2eebcbc52703431b96865b833f1c2
Ronlnn <artking01kg@gmail.com>
2025-08-11 17:55:54 +0300

    alacrity settings

--- home-manager/alacritty.nix
+++ home-manager/alacritty.nix
@@ -4,11 +4,19 @@
   # aura
   # dracula
   # tokyo_night
+  # horizon_dark
   programs.alacritty = {
     enable = true;
     theme = "horizon_dark";
     settings = {
       window.opacity = 0.95;
+      font = {
+        normal = {
+          family = "FiraCode Nerd Font";
+          style = "Regular";
+        };
+        size = 16;
+      };
     };
   };
 }
