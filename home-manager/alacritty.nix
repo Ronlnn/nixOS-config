5b485e64150f4ad2ac131570c1d9dfaded137a26
Ronlnn <artking01kg@gmail.com>
2025-08-11 17:37:42 +0300

    ala

--- home-manager/alacritty.nix
+++ home-manager/alacritty.nix
@@ -3,10 +3,10 @@
   # My favorite themes:
   # aura
   # dracula
-  #
+  # tokyo_night
   programs.alacritty = {
     enable = true;
-    theme = "tokyo_night";
+    theme = "citylights";
     settings = {
       window.opacity = 0.95;
     };
