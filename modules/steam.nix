04321cc14d247dae5d07d367dce2f8de8e165c93
Ronlnn <artking01kg@gmail.com>
2025-08-14 15:13:01 +0300

    ff

--- modules/steam.nix
+++ modules/steam.nix
@@ -1,18 +1,18 @@
-# {pkgs, ...}:
-# {
-#   programs.steam.enable = true;
-#   programs.steam.gamescopeSession.enable = true;
-#   programs.gamemode.enable = true;
+{pkgs, ...}:
+{
+  programs.steam.enable = true;
+  programs.steam.gamescopeSession.enable = true;
+  programs.gamemode.enable = true;
 
-#   environment.systemPackages = with pkgs; [
-#     protonup
-#     protontricks
-#     protonplus
-#     protonup-qt
-#   ];
+  environment.systemPackages = with pkgs; [
+    protonup
+    protontricks
+    protonplus
+    protonup-qt
+  ];
 
-#   environment.sessionVariables = {
-#     STEAM_EXTRA_COMPAT_TOOLS_PATHS =
-#       "/home/roninn/.steam/root/compatibilitytools.d";
-#   };
-# }
+  environment.sessionVariables = {
+    STEAM_EXTRA_COMPAT_TOOLS_PATHS =
+      "/home/roninn/.steam/root/compatibilitytools.d";
+  };
+}
